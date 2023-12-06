65e327bf26e3c43f12f42057af47c63507ed0c44
ntn0765907752 <nghiant21406@st.uel.edu.vn>
2023-12-06 21:43:27 +0700

    create layout

--- CLIENT/src/app/app-routing.module.ts
+++ CLIENT/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: "/drink", pathMatch: 'full'},
+  {path:'list-product', component:AddProductComponent},
+  // {path:'drink/:id', component:DrinksDetailComponent},
+  // {path:'food', component:FoodComponent},
+  {path:'**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- CLIENT/src/app/app.module.ts
+++ CLIENT/src/app/app.module.ts
@@ -5,17 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { HeaderAdminComponent } from './header-admin/header-admin.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddProductComponent
+    AddProductComponent,
+    HeaderComponent,
+    HeaderAdminComponent,
+    FooterComponent,
+    SidebarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule//Quan tọng, k có là k chạy
   ],
   providers: [],
   bootstrap: [AppComponent]
