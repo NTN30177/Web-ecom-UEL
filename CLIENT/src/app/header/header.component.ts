a3369868e5969efdb34bd8bee716c851b14d8f81
ntn0765907752 <nghiant21406@st.uel.edu.vn>
2023-12-16 00:53:01 +0700

    Header + footer

--- CLIENT/src/app/header/header.component.ts
+++ CLIENT/src/app/header/header.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { SearchService } from './header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css', './styles.css']
 })
 export class HeaderComponent {
+  isSearchFormActive: boolean = false;
+  isMainMenuOpen: boolean = false;
+  submenuOpen: boolean = false; 
 
+  constructor(private elRef: ElementRef) {}
+
+  toggleSearchForm(): void {
+    this.isSearchFormActive = !this.isSearchFormActive;
+  }
+
+  handleQuickSearchClick(event: Event): void {
+    event.stopPropagation();
+  }
+
+  toggleMainMenu(): void {
+    this.isMainMenuOpen = !this.isMainMenuOpen;
+  }
+
+  toggleSubmenu(): void {
+    this.submenuOpen = !this.submenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: Event): void {
+    if (!this.elRef.nativeElement.contains(event.target)) {
+      this.isSearchFormActive = false;
+      this.isMainMenuOpen = false;
+      this.submenuOpen = false; 
+    }
+  }
 }
