0838dea2038d60b9f7a81f55dae22727ec92779d
ntn0765907752 <nghiant21406@st.uel.edu.vn>
2023-12-15 21:57:53 +0700

    update template SERVER

--- SERVER/src/routes/index.js
+++ SERVER/src/routes/index.js
@@ -1,11 +1,18 @@
 
 const productRouter = require('./product');
+const adminRouter = require('./admin');
+const userRouter = require('./user');
 
 
 function route(app) {
 
     app.use('/product', productRouter);
-    app.use('/admin', productRouter);
+    app.use('/admin', adminRouter);
+    app.use('/cart',  cartRouter);
+    app.get('/error', function (req, res) {
+        res.render('404');
+    });
+    app.use('',  userRouter);
 
 }
 
