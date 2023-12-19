24dfda99297d6da898b996270a8efd3d3932e928
LPNhi <nhilbp21406@st.uel.edu.vn>
2023-12-19 14:21:16 +0700

    fix header&sidebar icon

--- SERVER/src/routes/cart.js
+++ SERVER/src/routes/cart.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const cartController = require('../app/controllers/CartController');
 // const upload = multer({ storage: storage });
 
-router.post(
+// router.post(
 
 module.exports = router;
