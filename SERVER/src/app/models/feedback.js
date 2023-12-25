d91c8b179eb32872633f66b9af13de06661a2f02
ntn0765907752 <nghiant21406@st.uel.edu.vn>
2023-12-25 10:06:48 +0700

    done order

---
+++ SERVER/src/app/models/feedback.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const slug = require("mongoose-slug-updater");
+mongoose.plugin(slug);
+
+  
+const FeedbackSchema = new mongoose.Schema({
+    idUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content:{
+      type:string
+    }
+  });
+  
+
+
+const Feedback = mongoose.model("Feedback", FeedbackSchema);
+module.exports = {
+    Feedback,
+}  
